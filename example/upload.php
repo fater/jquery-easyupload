58ea59fb7dfa098266c54b1516a922733213de08
Evgenii Chaikin <fat_hack@rambler.ru>
2015-11-11 04:22:28 +0600

    Update to version 1.1.0

--- example/upload.php
+++ example/upload.php
@@ -1,6 +1,14 @@
 <?php
 $json = [];
-$json['result'] = 'Все файлы загружены';
+$json['result'] = 'All files are uploaded!';
 $json['action'] = 'some_action';
+
+$json['data'] = [];
+foreach($_FILES as $item)
+{
+	$json['data'][] = array('name' => $item['name'], 'size' => $item['size']);
+}
+
+$json['return_data_to_client'] = $_POST;
 
 echo json_encode ($json);
--- src/jquery.easyupload.js
+++ src/jquery.easyupload.js
@@ -1,6 +1,6 @@
 /*
  * Module: jQuery Easy Upload Plugin
- * Version: 1.0.0
+ * Version: 1.1.0
  * Author: Chaikin Evgenii
  * Release date: 10 Nov 2015
  * Updated: 11 Nov 2015
@@ -8,56 +8,76 @@
  * Dependence: jQuery
  * */
 
-
 (function ($)
 {
 	var settings =
 	{
-
+		url: document.URL,
+		data: {},
+		//max_file_size: 0,
+		//max_total_size: 0,
+		file_name: 'file',
+		on_before_upload: function(info){},
+		on_progress: function(percent){},
+		on_upload_finish: function(data){},
+		on_upload_error: function(percent){}
 	};
 
 	$.fn.easyupload = function(options)
 	{
-		//settings.element = this;
+		settings = $.extend(settings, options);
+
 		$(document).ready(function ()
 		{
 			$(this).on('change', function(e)
 			{
+				e.stopPropagation();
+
+				var total_size = 0;
+				var files_info = {};
 				var data = new FormData();
-				$.each(e.target.files, function(key, value)
+
+				// Read files and prepare to submit
+				$.each(e.target.files, function(k, v)
 				{
-					data.append(key, value);
-					//console.log (key, value);
+					data.append(k, v);
+					files_info[k] = {size: v.size, name: v.name, type: v.type};
+					total_size += v.size;
 				});
 
+				// Append extra data to submit
+				$.each(settings.data, function (k, v)
+				{
+					data.append(k, v);
+				});
+				settings.on_before_upload.call(this, {files: files_info, total_size: total_size});
+
+				// Ajax submit
 				$.ajax({
-					url: 'upload.php',
+					url: settings.url,
 					type: 'POST',
 					data: data,
 					cache: false,
 					dataType: 'json',
-					processData: false, // Don't process the files, we're using FormData
-					contentType: false, // Set content type to false as jQuery will tell the server its a query string request
-					success: function(data, textStatus, jqXHR){
-						//self.processSubmit(event, data);
-						console.log ('Files uploaded sucessfully ', data);
+					processData: false,
+					contentType: false,
+					complete: function(data){},
+					success: function(data)
+					{
+						settings.on_upload_finish.call(this, data);
 					},
-					error: function(jqXHR, textStatus, errorThrown){
-						console.log ('Files uploaded Error');
-						//self.settings.error(jqXHR, textStatus, errorThrown);
+					error: function(data)
+					{
+						settings.on_upload_error.call(this, data);
 					},
 					xhr: function()
 					{
 						var xhr = $.ajaxSettings.xhr();
 						xhr.upload.onprogress = function(evt)
 						{
-							console.log('Загружено: ', evt.loaded / evt.total * 100, '%');
+							settings.on_progress.call(this, parseInt(evt.loaded / evt.total * 100));
 						};
-						// set the onload event handler
-						xhr.upload.onload = function(){
-							console.log('Все файлы загружены');
-						};
-						// return the customized object
+						xhr.upload.onload = function(){};
 						return xhr ;
 					}
 				});
